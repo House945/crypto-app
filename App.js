8fd2258997ef7c5d2658a734a8f3b166d0d1dd4d
Sebastian <31987608+House945@users.noreply.github.com>
2022-01-31 19:45:10 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -37,7 +37,7 @@ const App = () => {
       
       <div className="footer"  >
         <Typography.Title level={5} style={{ color:'white', textAllign:'center'}}>
-            Cryptoverse <br/>
+            Cryptoverse<br/>
             All rights reserved
         </Typography.Title>
         <Space>
---
+++ services/cryptoApi.js
@@ -0,0 +1,24 @@
+import {createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '6f1801d40amshb322267b816a2b5p1920a4jsn95e31d2e9755'
+};
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com/';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders});
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
