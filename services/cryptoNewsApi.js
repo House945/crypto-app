db202c2a48e539ae70812e7c4914fd9455c590ce
Sebastian <31987608+House945@users.noreply.github.com>
2022-02-02 20:58:37 +0100

    Add files via upload

---
+++ services/cryptoNewsApi.js
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoNewsHeaders = {
+  "x-bingapis-sdk": "true",
+  "x-rapidapi-host": "bing-news-search1.p.rapidapi.com",
+  "x-rapidapi-key": "6f1801d40amshb322267b816a2b5p1920a4jsn95e31d2e9755",
+};
+
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders});
+
+export const cryptoNewsApi = createApi({
+    reducerPath: 'cryptoNewsApi',
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints: (builder) => ({
+        getCryptoNews: builder.query({
+            query: ({newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+        })
+    })
+});
+
+export const {useGetCryptoNewsQuery} = cryptoNewsApi;
